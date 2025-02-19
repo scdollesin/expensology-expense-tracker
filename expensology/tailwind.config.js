88b8a4df081ff787424481c7e02ea530844643d9
scdollesin <scdollesin@up.edu.ph>
2025-02-19 20:35:51 +0800

    fix(ui): search bar and view toggle overlap

--- expensology/tailwind.config.js
+++ expensology/tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'xxs': '280px',
+      'xs': '375px',
+      'ss': '425px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '1xl': '1440px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+      '4xl': '2560px',
+    },
     extend: {
       colors: {
         light_blue: {
