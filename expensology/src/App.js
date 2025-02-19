29e87bae2cef7f74412c967ec48e7c9bb76760f0
scdollesin <scdollesin@up.edu.ph>
2025-02-19 14:55:51 +0800

    refactor: modularized header

--- expensology/src/App.js
+++ expensology/src/App.js
@@ -1,18 +1,10 @@
-import logo from './expensology-rounded-icon.png';
 import './App.css';
+import Header from './Header/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="App-brand-container">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <div>
-            <h1 className="App-name">Expensology</h1>
-            <h5 className="App-tagline">Smart Expense Tracking, Simplified.</h5>
-          </div>
-        </div>
-      </header>
+      <Header/>
       <main className="App-main">
 
       </main>
---
+++ expensology/src/Header/Header.js
@@ -0,0 +1,19 @@
+import './Header.css';
+import logo from './expensology-rounded-icon.png';
+
+
+function Header() {
+    return (
+        <header className="App-header">
+        <div className="App-brand-container">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <div>
+            <h1 className="App-name">Expensology</h1>
+            <h5 className="App-tagline">Smart Expense Tracking, Simplified.</h5>
+          </div>
+        </div>
+      </header>
+    );
+}
+
+export default Header;
